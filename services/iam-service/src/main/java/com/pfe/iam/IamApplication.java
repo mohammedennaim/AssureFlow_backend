64a7d8967bb6efa77313c8984902e7f2708f8157
mohammedennaim <mohammedennaim99@gmail.com>
2026-03-02 00:10:53 +0000

    fix: Update healthcheck command and parameters in docker-compose.yml; add component scan in IamApplication

--- services/iam-service/src/main/java/com/pfe/iam/IamApplication.java
+++ services/iam-service/src/main/java/com/pfe/iam/IamApplication.java
@@ -2,8 +2,10 @@ package com.pfe.iam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.pfe.iam", "com.pfe.commons"})
 public class IamApplication {
 
     public static void main(String[] args) {
